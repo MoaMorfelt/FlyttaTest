6cad9bcaa5df9495d3ae6f18edeb51c87a2e1017
Moa Morfelt <morfelt.moa@gmail.com>
2021-11-10 11:53:13 +0100

    Initial Commit

---
+++ FlyttaTest/FlyttaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyttaTestApp.swift
+//  FlyttaTest
+//
+//  Created by Moa Morfelt on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct FlyttaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
